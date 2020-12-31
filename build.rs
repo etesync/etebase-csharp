8b966922d076577d8eb5af5fde3c135727223456
Tom Hacohen <tom@stosb.com>
2020-12-31 17:37:45 +0200

    Fix bindings generation.

--- build.rs
+++ build.rs
@@ -7,11 +7,10 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let in_src = Path::new("src").join("glue.rs.in");
     let out_src = Path::new(&out_dir).join("glue.rs");
-    let lib_path = PathBuf::from(&out_dir);
 
-    let config = DotNetConfig::new("etebase_csharp".to_owned(), lib_path);
+    let config = DotNetConfig::new("etebase_csharp".to_owned(), PathBuf::from("target/dotnet-out/"));
     let swig_gen = rust_swig::Generator::new(LanguageConfig::DotNetConfig(config))
         .rustfmt_bindings(true);
-    swig_gen.expand("csharp bindings", &in_src, &out_src);
+    swig_gen.expand("etebase_csharp_native", &in_src, &out_src);
     println!("cargo:rerun-if-changed={}", in_src.display());
 }
